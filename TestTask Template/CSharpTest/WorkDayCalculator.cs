d4b8c5371bc1279b57b7db3d0cb9f915e1997aed
Maks1970 <69193555+Maks1970@users.noreply.github.com>
2023-01-18 14:59:30 +0200

    Add files via upload

---
+++ TestTask Template/CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime sumarryday= new DateTime(1,1,1)  ;
+            int weekendsCount;
+
+            if (weekEnds != null)
+            {
+
+
+                if (weekEnds.Length > 1)
+                {
+                    // weekends in work day
+                    for (int i = 0; i < weekEnds.Length; i++)
+                    {
+                        if (weekEnds[i].StartDate < startDate.AddDays(dayCount))
+                        {
+                            weekendsCount = weekEnds[0].EndDate.Day - weekEnds[0].StartDate.Day;
+                            if (weekendsCount == 0) { weekendsCount = 1; }
+                            dayCount += weekendsCount;
+
+                        }
+                    }
+                    sumarryday = startDate.AddDays(dayCount);
+                }
+                if (weekEnds.Length == 1)
+                {
+
+                    // weekends in work day
+                    if (weekEnds[0].StartDate < startDate.AddDays(dayCount))
+                    {
+                        weekendsCount = weekEnds[0].EndDate.Day - weekEnds[0].StartDate.Day;
+                        if (weekendsCount == 0) { weekendsCount = 1; }
+                        dayCount += weekendsCount;
+
+                    }
+                    sumarryday = startDate.AddDays(dayCount);
+                }
+            } else { sumarryday = startDate.AddDays(dayCount-1); }
+
+           // throw new NotImplementedException();
+           return sumarryday;
+        }
+    }
+}
